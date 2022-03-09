081efc2df5bb44f746059166ed2ef95f8546a058
Michael Morgan <morgan.mike55@gmail.com>
2022-03-08 21:50:33 -0500

    connected db to template

--- mkemorgn_web/views.py
+++ mkemorgn_web/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+from mkemorgn_web.mkemorgn.models import About
+
 
 def index(request):
     return render(request, "index.html")
 
 
 def about(request):
-    return render(request, "about.html")
+    about_page = About.objects.all()
+    return render(request, "about.html", {"about_page": about_page})
 
 
 def contact(request):
