1dcecacaf73ffaed6876aebe509815c8fc3deaa0
Michael Morgan <morgan.mike55@gmail.com>
2022-03-08 21:46:24 -0500

    connected db to template

--- mkemorgn_web/mkemorgn/models.py
+++ mkemorgn_web/mkemorgn/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class About(models.Model):
-    about_text = models.CharField(max_length=2000)
+    about_text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "about"
+
+    def __str__(self):
+        return self.about_text
